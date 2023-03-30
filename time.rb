708a247c28681b4d39351895a1c9a8b209a0b44c
Miguel Alexandre <62778352+MiguelCoder@users.noreply.github.com>
2023-03-30 10:45:19 -0300

    Mais aulas e renomeações

---
+++ time.rb
@@ -0,0 +1,13 @@
+6.times{print "Exemplo 1 "}
+6.times{print "Exemplo 1"}
+6.times{puts "Exemplo 1"}
+
+#Exemplo 2:
+4.times do
+    puts "Exemplo 2"
+end
+
+#Exemplo3:
+21.times do |contador|
+puts "#{contador}"
+end
